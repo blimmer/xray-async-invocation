a61882025dbaa3faff6a8171d0fa437dcf80c780
Ben Limmer <blimmer@ginger.io>
2021-11-30 13:54:28 -0700

    Initial commit

---
+++ test/xray-async-invocation.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as XrayAsyncInvocation from '../lib/xray-async-invocation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/xray-async-invocation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new XrayAsyncInvocation.XrayAsyncInvocationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
