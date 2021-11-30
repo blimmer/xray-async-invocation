1a8eb7346c2255bce10cc09cc431288c333e6689
Ben Limmer <blimmer@ginger.io>
2021-11-30 16:26:48 -0700

    patch everything, not by client

--- lib/xray-async-invocation-stack.triggering.ts
+++ lib/xray-async-invocation-stack.triggering.ts
@@ -1,18 +1,20 @@
 import { Handler } from "aws-lambda";
-import { captureAWSClient } from "aws-xray-sdk";
-import { Lambda, S3 } from "aws-sdk";
+import { captureAWS } from "aws-xray-sdk";
+import _AWS from "aws-sdk";
+
+const AWS = captureAWS(_AWS);
 
 const handler: Handler = async (event, context) => {
   console.info("This is the triggering lambda");
   console.info(JSON.stringify(event));
 
   console.info("Listing buckets to generate a segment...");
-  const s3 = captureAWSClient(new S3());
+  const s3 = new AWS.S3();
   const { Buckets } = await s3.listBuckets().promise();
   console.info(`Found ${Buckets?.length || 0} S3 buckets.`);
 
   console.info("Async invoking the lambda...");
-  const lambda = captureAWSClient(new Lambda());
+  const lambda = new AWS.Lambda();
   await lambda
     .invokeAsync({
       FunctionName: process.env.TRIGGERED_FUNCTION_NAME!,
